963451ec5a07a0eb89f74b04a628638074c665e4
LeoFdz <lfernandez79@github.com>
2020-01-24 17:14:34 -0600

    First draft for pwdGenerator app

---
+++ Develop/script.js
@@ -0,0 +1,24 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+var lowCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var numbers = [1,2,3,4,5,6,7,8,9,0];
+var symb = ["~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+"];
+
+for (let index = 0; index < array.length; index++) {
+  const element = array[index];
+  
+}
