20e2a9f8e8c49c763e4a376400391c148fb94055
LeoFdz <lfernandez79@github.com>
2020-01-25 17:45:42 -0600

    connected all var in script to provide random password

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,21 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+var lowCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var numbers = ["1","2","3","4","5","6","7","8","9","0"];
+var symb = ["~","!","@","#","$","%","^","&","*","(",")","_","+"];
+
+var getLow = lowCase[Math.floor(Math.random() * lowCase.length)];
+var getUpper = upperCase[Math.floor(Math.random() * upperCase.length)];
+var getNum = numbers[Math.floor(Math.random() * numbers.length)];
+var getSymb = symb[Math.floor(Math.random() * symb.length)];
+
+console.log(getLow, getUpper, getNum, getSymb);
+
+// Write password to the #password input, NOT define yet.
 function writePassword() {
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
@@ -13,12 +26,17 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-var lowCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var numbers = [1,2,3,4,5,6,7,8,9,0];
-var symb = ["~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+"];
+function defineCriteriaForPassword() {
 
-for (let index = 0; index < array.length; index++) {
-  const element = array[index];
-  
+  // We need to generate random number to find a random character
 }
+
+
+
+
+
+// for (let index = 0; index < array.length; index++) {
+//   const element = array[index];
+  
+// }
+
