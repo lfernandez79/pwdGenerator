ac34396b12f48235ae63deb9c293549eb8c475da
LeoFdz <lfernandez79@github.com>
2020-01-26 16:59:43 -0600

    Added prompts and confoirms

--- Develop/script.js
+++ Develop/script.js
@@ -1,30 +1,38 @@
-// Assignment Code
+
 var generateBtn = document.querySelector("#generate");
 
-var lowCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-var numbers = ["1","2","3","4","5","6","7","8","9","0"];
-var symb = ["~","!","@","#","$","%","^","&","*","(",")","_","+"];
+var charOptions = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+"];
 
-var getLow = lowCase[Math.floor(Math.random() * lowCase.length)];
-var getUpper = upperCase[Math.floor(Math.random() * upperCase.length)];
-var getNum = numbers[Math.floor(Math.random() * numbers.length)];
-var getSymb = symb[Math.floor(Math.random() * symb.length)];
+// Pick random Character. But I cannot get it to get 8 characters minimal it just one at the time.
+var passLength = 8;
+var pwd = "";
+var getCharOptions = charOptions[Math.floor(Math.random() * charOptions.length)];
 
-console.log(getLow, getUpper, getNum, getSymb);
+for (var i = 0; i <= charOptions.length; i++) {
+  var pwd = charOptions[i];
+  
+}
+
+console.log(getCharOptions);
 
 // Write password to the #password input, NOT define yet.
 function writePassword() {
-
+  
+  var getPwdLenght = Number(prompt("Password length between 8 and 128, please?", ""));
+  var getLowUpperCase = confirm("You need low and upper cases!");
+  var getSymbNumb = confirm("Also you need Numbers and Symbols, not opnional.");
   var password = generatePassword();
+  
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// What fuction to call when click button.
+generateBtn.addEventListener("click", writePassword);{
+
+  
+}
 
 function defineCriteriaForPassword() {
 
@@ -35,8 +43,4 @@ function defineCriteriaForPassword() {
 
 
 
-// for (let index = 0; index < array.length; index++) {
-//   const element = array[index];
-  
-// }
 
