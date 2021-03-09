5839e98e87c791a34a46bfc97e6826d46d04b68f
LeoFdz <lfernandez79@gmail.com>
2021-03-09 17:33:06 -0600

    added assets to manifest and serviceworker

---
+++ serviceWorker.js
@@ -0,0 +1,20 @@
+const FILES_TO_CACHE = [
+    "/",
+    "/index.html",
+    "/manifest.json",
+    "/public/assets/style/style.css",
+    "/public/assets/style/bootstrap.css",
+    "/public/assets/JS/script.js",
+    "/public/assets/JS/hsimp.min.js",
+    "/public/assets/image/mobilePG.png",
+    "/public/assets/image/PG.png",
+    "/public/assets/image/PG2.png",
+    "/public/assets/image/icons/favicon_io/android-chrome-192x192.png",
+    "/public/assets/image/icons/favicon_io/android-chrome-512x512.png",
+    "/public/assets/image/icons/favicon_io/apple-touch-icon.png",
+    "/public/assets/image/icons/favicon_io/favicon-16x16.png",
+    "/public/assets/image/icons/favicon_io/favicon-32x32.png",
+    "/public/assets/image/icons/favicon_io/favicon.ico",
+    "/public/assets/image/icons/favicon_io/site.webmanifest",
+
+];
